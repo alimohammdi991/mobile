bfea40c7df537571ad8c07919f085a08768e8345
alifcpr <esfahani.aliyahoo@gmail.com>
2023-08-09 13:13:46 +0330

    change product slice code

--- src/slices/productSlice.js
+++ src/slices/productSlice.js
@@ -14,7 +14,12 @@ const initialState = {
   filteredProduct: [],
   loading: true,
   error: false,
+  filters: {
+    colors: [],
+    price: 80000000,
+  },
 };
+
 
 const productSlice = createSlice({
   name: "product",
@@ -23,22 +28,46 @@ const productSlice = createSlice({
     setFilter: (state, action) => {
       const { type, payload } = action.payload;
 
-      const filteredProduct = state.products.filter((product) => {
-        switch (type) {
-          case "FILTER_COLOR":
-            console.log("log here")
-            const colors = payload;
-            if (colors.length === 0) {
-              return true;
-            } else {
-              return product.colors.some((color) => colors.includes(color));
-            }
-          case "FILTER_PRICE_RANGE":
-            const price = Number(payload);
-            return product.price <= price;
-        }
-      });
-      state.filteredProduct = filteredProduct;
+      state.filters.colors = type === "FILTER_COLOR" ? payload : [];
+      state.filters.price = type === "FILTER_PRICE_RANGE" ? Number(payload) : 80000000;
+
+      console.log(Number(state.filters.price) !== 80000000, !!state.filters.colors.length)
+      if (Number(state.filters.price) !== 80000000 || state.filters.colors.length) {
+        console.log("in koni run mishe")
+        const filteredProduct = state.filteredProduct.filter((product) => {
+          switch (type) {
+            case "FILTER_COLOR":
+              console.log("log here");
+              const colors = payload;
+              if (colors.length === 0) {
+                return true;
+              } else {
+                return product.colors.some((color) => colors.includes(color));
+              }
+            case "FILTER_PRICE_RANGE":
+              const price = Number(payload);
+              return product.price <= price;
+          }
+        });
+      } else {
+        const filteredProduct = state.products.filter((product) => {
+          console.log("in koni dige ham run mishe");
+          switch (type) {
+            case "FILTER_COLOR":
+              console.log("log here");
+              const colors = payload;
+              if (colors.length === 0) {
+                return true;
+              } else {
+                return product.colors.some((color) => colors.includes(color));
+              }
+            case "FILTER_PRICE_RANGE":
+              const price = Number(payload);
+              return product.price <= price;
+          }
+        });
+        state.filteredProduct = filteredProduct;
+      }
     },
   },
   extraReducers: (builder) => {
